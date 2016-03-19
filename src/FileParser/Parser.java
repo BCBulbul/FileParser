9dbd73fc054b8f8a5e5a9e7732211d9d262706cb
burakcan <bcbulbul1606@gmail.com>
2016-03-19 19:08:06 +0200

    Latest commit

--- src/FileParser/Parser.java
+++ src/FileParser/Parser.java
@@ -125,7 +125,7 @@ public class Parser
                     fileContent+=div.text();
                   }
                   
-                  getParsedData.add(fileContent);
+                   getParsedData.add(fileContent);
                   
                 }  
                   
